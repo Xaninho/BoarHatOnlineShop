98ff1721432ccd3853408ede272bca33c395f934
Xaninho <neves.27.17@gmail.com>
2020-09-15 12:13:47 +0100

    Top Sale Carousel

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
 
-    //banner owl carousel
-
+    // banner owl carousel
     $("#banner-area .owl-carousel").owlCarousel({
         dots: true,
-        items: 1,
-        navigation: true
+        items: 1
+    });
+
+    // top sale owl carousel
+    // top sale owl carousel
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
     });
 
 });
