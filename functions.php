f807d1c102e9f63116f4bdc14ae169918996d066
Xaninho <neves.27.17@gmail.com>
2020-10-05 10:59:52 +0100

    Fetch Data for Special Price

--- functions.php
+++ functions.php
@@ -3,5 +3,11 @@
 // require MySQL Connection
 require ('./database/DBController.php');
 
+// require Product Class
+require ('./database/Product.php');
+
 // DBController object
 $db = new DBController();
+
+// Product object
+$product = new Product($db);
