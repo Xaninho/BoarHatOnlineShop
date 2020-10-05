4828706bf1d576f4644b198fab80727e8c2821fe
Xaninho <neves.27.17@gmail.com>
2020-10-05 15:55:50 +0100

    Add to Cart

--- components/_special-price.php
+++ components/_special-price.php
@@ -1,54 +1,65 @@
 <!-- SPECIAL PRICE -->
 <?php
-  $brand = array_map(function ($pro){return $pro['item_brand']; }, $product_suffle);
+  $brand = array_map(function ($pro) {return $pro['item_brand'];}, $product_suffle);
   $unique = array_unique($brand);
   sort($unique);
   shuffle($product_suffle);
+
+  // request method post
+  if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if (isset($_POST['special_sale_submit'])){
+        // call method addToCart
+        $Cart->addToCart($_POST['user_id'], $_POST['item_id']);
+    }
+  }
 ?>
 
 <section id="special-price">
 
-<div class="container">
-  <h4 class="font-rubik font-size-20">Special Price</h4>
-  <div id="filters" class="button-group text-right font-rubik font-size-16">
-    <button class="btn is-checked" data-filter="*">All Brand</button>
+  <div class="container">
+    <h4 class="font-rubik font-size-20">Special Price</h4>
+    <div id="filters" class="button-group text-right font-rubik font-size-16">
+      <button class="btn is-checked" data-filter="*">All Brand</button>
 
-    <?php
-      array_map(function ($brand){
+      <?php
+      array_map(function ($brand) {
 
         printf('<button class="btn" data-filter=".%s">%s</button>', $brand, $brand);
-
       }, $unique)
-    ?>
-  </div>
+      ?>
+    </div>
 
-  <div class="grid">
+    <div class="grid">
 
-    <?php array_map(function($item){ ?>
-    <div class="grid-item border <?php echo $item['item_brand']; ?>">
-      <div class="item py-2" style="width: 200px;">
-        <div class="product font-rale">
-          <a href="<?php printf('%s?item_id=%s', 'product.php', $item['item_id']) ?>"><img src="<?php echo $item['item_image']; ?>" alt="product1" class="img-fluid"></a>
-          <div class="text-center">
-            <h6><?php echo $item['item_name']; ?></h6>
-            <div class="rating text-warning font-size-12">
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="fas fa-star"></i></span>
-              <span><i class="far fa-star"></i></span>
+      <?php array_map(function ($item) { ?>
+        <div class="grid-item border <?php echo $item['item_brand']; ?>">
+          <div class="item py-2" style="width: 200px;">
+            <div class="product font-rale">
+              <a href="<?php printf('%s?item_id=%s', 'product.php', $item['item_id']) ?>"><img src="<?php echo $item['item_image']; ?>" alt="product1" class="img-fluid"></a>
+              <div class="text-center">
+                <h6><?php echo $item['item_name']; ?></h6>
+                <div class="rating text-warning font-size-12">
+                  <span><i class="fas fa-star"></i></span>
+                  <span><i class="fas fa-star"></i></span>
+                  <span><i class="fas fa-star"></i></span>
+                  <span><i class="fas fa-star"></i></span>
+                  <span><i class="far fa-star"></i></span>
+                </div>
+                <div class="price py-2">
+                  <span>$<?php echo $item['item_price']; ?></span>
+                </div>
+                <form method="post">
+                  <input type="hidden" name="item_id" value="<?php echo $item['item_id']; ?>">
+                  <input type="hidden" name="user_id" value="<?php echo 1; ?>">
+                  <button type ="submit" name="special_sale_submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                </form>
+              </div>
             </div>
-            <div class="price py-2">
-              <span>$<?php echo $item['item_price']; ?></span>
-            </div>
-            <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
           </div>
         </div>
-      </div>
-    </div>
-    <?php }, $product_suffle) ?>
+      <?php }, $product_suffle) ?>
 
+    </div>
   </div>
-</div>
 
 </section>
--- components/_top-sale.php
+++ components/_top-sale.php
@@ -1,42 +1,54 @@
  <!-- TOP SALE -->
 
-<?php
+ <?php
   $product_suffle = $product->getData();
   shuffle($product_suffle);
-?>
 
-<section id="top-sale">
+    // request method post
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+      if (isset($_POST['top_sale_submit'])){
+          // call method addToCart
+          $Cart->addToCart($_POST['user_id'], $_POST['item_id']);
+      }
+    }
+  ?>
 
-<div class="container py-5">
-  <h4 class="font-rubik font-size-20">Top Sale</h4>
-  <hr>
-  <div class="owl-carousel owl-theme">
+ <section id="top-sale">
 
-    <?php foreach ($product_suffle as $item) { ?>
+   <div class="container py-5">
+     <h4 class="font-rubik font-size-20">Top Sale</h4>
+     <hr>
+     <div class="owl-carousel owl-theme">
 
-    <div class="item py-2">
-      <div class="product font-rale">
-        <a href="<?php printf('%s?item_id=%s', 'product.php', $item['item_id']) ?>"><img src="<?php echo $item['item_image']; ?>" alt="product1" class="img-fluid"></a>
-        <div class="text-center">
-          <h6><?php echo $item['item_name']; ?></h6>
-          <div class="rating text-warning font-size-12">
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="fas fa-star"></i></span>
-            <span><i class="far fa-star"></i></span>
-          </div>
-          <div class="price py-2">
-            <span><?php echo $item['item_price']; ?></span>
-          </div>
-          <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-        </div>
-      </div>
-    </div>
+       <?php foreach ($product_suffle as $item) { ?>
 
-    <?php } ?>
+         <div class="item py-2">
+           <div class="product font-rale">
+             <a href="<?php printf('%s?item_id=%s', 'product.php', $item['item_id']) ?>"><img src="<?php echo $item['item_image']; ?>" alt="product1" class="img-fluid"></a>
+             <div class="text-center">
+               <h6><?php echo $item['item_name']; ?></h6>
+               <div class="rating text-warning font-size-12">
+                 <span><i class="fas fa-star"></i></span>
+                 <span><i class="fas fa-star"></i></span>
+                 <span><i class="fas fa-star"></i></span>
+                 <span><i class="fas fa-star"></i></span>
+                 <span><i class="far fa-star"></i></span>
+               </div>
+               <div class="price py-2">
+                 <span><?php echo $item['item_price']; ?></span>
+               </div>
+               <form method="post">
+                  <input type="hidden" name="item_id" value="<?php echo $item['item_id']; ?>">
+                  <input type="hidden" name="user_id" value="<?php echo 1; ?>">
+                  <button type ="submit" name="top_sale_submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                </form>
+             </div>
+           </div>
+         </div>
 
-  </div>
-</div>
+       <?php } ?>
 
-</section>
+     </div>
+   </div>
+
+ </section>
