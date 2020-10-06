e169ebe7a505a2d9487deeb085fba352b9a5ed8d
Xaninho <neves.27.17@gmail.com>
2020-10-06 11:49:58 +0100

    Delete Items from Shopping Cart

--- components/_shopping-cart.php
+++ components/_shopping-cart.php
@@ -1,4 +1,13 @@
  <!-- SHOPPING CART -->
+ <?php
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if (isset($_POST['delete-cart-submit'])){
+            $deletedrecord = $Cart->deleteCart($_POST['item_id']);
+        }
+    }
+
+ ?>
  <section id="cart" class="py-3 mb-5">
             <div class="container-fluid w-75">
                 <h5 class="font-baloo font-size-20">Shopping Cart</h5>
@@ -39,8 +48,10 @@
                                         <input type="text" data-id="pro1" class="qty_input border px-2 w-50 bg-light" disabled value="1" placeholder="1">
                                         <button class="qty-down border bg-light" data-id="pro1" ><i class="fas fa-angle-down"></i></button>
                                     </div>
-                                        <button type="submit" class="btn font-baloo text-danger px-3 border-right">Delete</button>
-                                        <button type="submit" class="btn font-baloo text-danger">Save for Later</button>
+                                        <form method="post">
+                                            <input type="hidden" name ="item_id" value="<?php echo $item['item_id']; ?>">
+                                            <button type="submit" name="delete-cart-submit" class="btn font-baloo text-danger px-3 border-right">Delete</button>
+                                        </form>
                                 </div>
                             </div>
 
